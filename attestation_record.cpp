7f2df3c4f16bd2af9827570db52372e02a8e016c
Shivaprasad Hongal <shongal@codeaurora.org>
2016-10-18 15:58:51 -0700

    keymaster: Enable Soter for Android N
    Add Soter tags for enforcements
    
    Change-Id: Ied40c218b3b331e17cd6ca32d5379bb722ff3ab8

--- attestation_record.cpp
+++ attestation_record.cpp
@@ -328,6 +328,18 @@ static keymaster_error_t build_auth_list(const AuthorizationSet& auth_list, KM_A
         case KM_TAG_APPLICATION_ID:
             string_ptr = &record->application_id;
             break;
+
+        /* Soter Tags begins */
+        case KM_TAG_SOTER_IS_FROM_SOTER:
+        case KM_TAG_SOTER_IS_AUTO_SIGNED_WITH_ATTK_WHEN_GET_PUBLIC_KEY:
+        case KM_TAG_SOTER_IS_AUTO_SIGNED_WITH_COMMON_KEY_WHEN_GET_PUBLIC_KEY:
+        case KM_TAG_SOTER_AUTO_SIGNED_COMMON_KEY_WHEN_GET_PUBLIC_KEY:
+        case KM_TAG_SOTER_AUTO_ADD_COUNTER_WHEN_GET_PUBLIC_KEY:
+        case KM_TAG_SOTER_IS_SECMSG_FID_COUNTER_SIGNED_WHEN_SIGN:
+        case KM_TAG_SOTER_USE_NEXT_ATTK:
+        case KM_TAG_SOTER_UID:
+        case KM_TAG_SOTER_AUTO_SIGNED_COMMON_KEY_WHEN_GET_PUBLIC_KEY_BLOB:
+            continue;
         }
 
         keymaster_tag_type_t type = keymaster_tag_get_type(entry.tag);
