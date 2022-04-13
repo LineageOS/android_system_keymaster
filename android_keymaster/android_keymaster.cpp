2dc09f37d882bfe15c0a18e93b0519ac7e9f2ab5
David Drysdale <drysdale@google.com>
2022-04-13 16:35:35 +0100

    Error log on key downgrade
    Make it more obvious when something attempts to use a key that is
    apparently from the future.
    
    Bug: 228893064
    Test: VtsAidlKeyMintTargetTest
    Change-Id: I37886b7e742898fe5363c7d43f543ee67ebea2e3

--- android_keymaster/android_keymaster.cpp
+++ android_keymaster/android_keymaster.cpp
@@ -66,6 +66,8 @@ keymaster_error_t CheckPatchLevel(const AuthorizationSet& tee_enforced,
         if (key_patchlevel < current_patchlevel) {
             return KM_ERROR_KEY_REQUIRES_UPGRADE;
         } else if (key_patchlevel > current_patchlevel) {
+            LOG_E("Key blob invalid! key patchlevel %lu is > current patchlevel %lu",
+                  (unsigned long)key_patchlevel, (unsigned long)current_patchlevel);
             return KM_ERROR_INVALID_KEY_BLOB;
         }
     }
